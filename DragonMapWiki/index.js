50dcd13e685710203f46cafdf5eb50d609f702bf
Kyohei <yamaguky@oregonstate.edu>
2024-12-06 18:40:52 -0800

    main page and make server done

---
+++ DragonMapWiki/index.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const goToWikiButton = document.getElementById('goToWiki');
+    goToWikiButton.addEventListener('click', () => {
+      console.log('Navigating to the Dragon Map Wiki...');
+    });
+  });
+  
--- server.js
+++ server.js
@@ -1,17 +1,40 @@
-var express = require("express")
+const express = require('express');
+const path = require('path');
+const { create } = require('express-handlebars'); 
+
+const app = express();
+const PORT = 3000;
+
+// Handlebars
+const hbs = create({
+  extname: '.handlebars', 
+  defaultLayout: 'main',
+  layoutsDir: path.join(__dirname, 'HandleBarsTemp/layouts'),
+});
+
+app.engine('handlebars', hbs.engine); 
+app.set('view engine', 'handlebars'); 
+app.set('views', path.join(__dirname, 'HandleBarsTemp')); 
+
+// Static dir
+app.use(express.static(path.join(__dirname, 'DragonMapWiki')));
+
+// Main 
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Welcome to Dragon Wiki' });
+});
+
+// Dragon Map Wiki
+app.get('/test', (req, res) => {
+  res.render('test', { title: 'Dragon Map Wiki' });
+});
+
+// 404
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404 - Page Not Found' });
+});
 
 
-
-var app = express()
-var port = process.env.PORT || 3002
-
-
-
-app.get("/", function (req, res, next) {
-
-    res.status(200).sendFile(__dirname + "/Lucy/test.html")
-})
-
-app.listen(port, function () {
-    console.log("== Server is listening on port", port)
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
